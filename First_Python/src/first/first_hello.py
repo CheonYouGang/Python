c99296a57431c1518d37aeb45b7cda65408b0de6
가족 <@DESKTOP-DVM88EI>
2018-05-14 00:17:48 +0900

    파이썬 출력과 연산

---
+++ First_Python/src/first/first_hello.py
@@ -0,0 +1,34 @@
+'''
+Created on 2018. 5. 13.
+
+@author: 가족
+'''
+#출력
+print("Hello Python")
+
+#단순 연산
+a=1
+b=2
+print(a+b)
+
+#문자 타입을(str) 숫자 타입(int)으로 변환하고 연산
+c='2'
+d=3
+e=int(c)
+print(e+d)
+
+#숫자 타입(int)을 문자타입(str)로 변환하고 연산
+f='3'
+g=4
+g=str(4)
+print(f+g)
+#===============================================================================
+# class JDBCEx2 {
+# +    public static void main(String args[]) {
+#     System.out.println("Hello Python");
+
+#    int a = 1;
+#    int b = 2;
+#    System.out.println(a+b);
+#     }
+#===============================================================================
